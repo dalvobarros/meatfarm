2e9056327600d89cbb8add8670952f269e2b4ff8
Dalvo Barros <dalvo.brs@gmail.com>
2020-02-25 06:38:55 -0300

    First Commit

---
+++ src/test/java/com/inovetecnologias/comercial/MeatfarmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovetecnologias.comercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatfarmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
